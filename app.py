a34e3384d0b6b129fb63cab47ec8119b065e4d83
aboud20-meet <aboud20@meet.mit.edu>
2018-11-22 18:42:39 +0200

    not done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from database import get_all_cats
+from database import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
@@ -10,5 +10,20 @@ def catbook_home():
     cats = get_all_cats()
     return render_template("home.html", cats=cats)
 
+@app.route('/cats/<int:id>')
+def cat_route(id):
+    name = get_cat_name(id)
+    return render_template(
+        'cat.html', n = id , m = name)
+
 if __name__ == '__main__':
    app.run(debug = True)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create_new_cat():
+	if request.method == 'GET':
+		return render_template("home.htnl")
+	else:
+		name=request.form['catname']
+		create_cat(name)     
+		return redirect('/')
